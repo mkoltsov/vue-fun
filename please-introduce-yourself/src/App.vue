dbb3309051a63368bde75d19e622e273d21a19d7
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2017-10-08 14:00:00 +0200

    simple vue app created

---
+++ please-introduce-yourself/src/App.vue
@@ -0,0 +1,63 @@
+//App.vue
+<template>
+  <div id="app" class="jumbotron">
+  <div class="container">
+      <h1>Hello! Nice to meet you!</h1>
+    <hr />
+    <form>
+        <div class="form-group">
+          <input class="form-control" maxlength="40" autofocus placeholder="Please introduce yourself :)" />
+        </div>
+        <div class="form-group">
+          <textarea class="form-control" placeholder="Leave your message!"  rows="3">
+          </textarea>
+        </div>
+        <button class="btn btn-primary" type="submit">Send</button>
+      </form>
+    <div class="card-group">
+    <div class="card" v-for="message in messages">
+      <div class="card-block">
+        <h5 class="card-title">{{ message.title }}</h5>
+        <p class="card-text">{{ message.text }}</p>
+        <p class="card-text"><small class="text-muted">Added on {{ message.timestamp }}</small></p>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+  import Firebase from 'firebase'
+
+  let config = {
+    apiKey: 'AIzaSyD9sbzFLMowLXsLDzgwGcebQyZMhLBrNB0',
+    authDomain: 'vuejstestproject-ee411.firebaseapp.com',
+    databaseURL: 'https://vuejstestproject-ee411.firebaseio.com',
+    projectId: 'vuejstestproject-ee411',
+    storageBucket: 'vuejstestproject-ee411.appspot.com',
+    messagingSenderId: '135223515034'
+  }
+
+  let app = Firebase.initializeApp(config)
+  let db = app.database()
+  let messagesRef = db.ref('messages')
+  export default {
+    name: 'app',
+    firebase: {
+      messages: messagesRef
+    }
+  }
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
